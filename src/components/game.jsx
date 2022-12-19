8212481fb677e0021ed15d566393e27ee5b3c5b8
BANYAWAT KRUYTHONG <banyawat.kry@gmail.com>
2022-12-19 11:09:30 +0700

    feat: pushing-state-down

---
+++ src/components/game.jsx
@@ -0,0 +1,41 @@
+import ColorSwatch from './color-swatch';
+import GameInput from './game-input';
+import GameStatus from './game-status';
+import generateRandomColor from '../lib/generate-random-color';
+import { useState } from 'react';
+
+const Game = () => {
+  const [colorGuess, setColorGuess] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState(() => generateRandomColor());
+  const [hasGuessed, setHasGuessed] = useState(false);
+  const [isWinner, setIsWinner] = useState(false);
+
+  if (hasGuessed) {
+    if (correctAnswer === colorGuess) {
+      setIsWinner(true);
+    }
+  }
+
+  return (<>
+    <ColorSwatch color={correctAnswer} />
+    <GameInput
+      value={colorGuess}
+      onChange={(e) => setColorGuess(e.target.value)}
+      onSubmit={() => setHasGuessed(true)}
+      disabled={hasGuessed}
+    />
+    <GameStatus isWinner={isWinner} hasGuessed={hasGuessed} />
+    <button
+      onClick={() => {
+        setCorrectAnswer(generateRandomColor());
+        setHasGuessed(false);
+        setColorGuess('');
+      }}
+      type={hasGuessed ? 'submit' : 'button'}
+    >
+      Reset Color
+    </button>
+    </>
+  )
+}
+export default Game;
