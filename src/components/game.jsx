995ac51cc8f2513c757c6a9647887cdeb2a6deb3
BANYAWAT KRUYTHONG <banyawat.kry@gmail.com>
2022-12-19 11:20:53 +0700

    feat: pulling-content-up

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -4,7 +4,7 @@ import GameStatus from './game-status';
 import generateRandomColor from '../lib/generate-random-color';
 import { useState } from 'react';
 
-const Game = () => {
+const Game = ({children}) => {
   const [colorGuess, setColorGuess] = useState('');
   const [correctAnswer, setCorrectAnswer] = useState(() => generateRandomColor());
   const [hasGuessed, setHasGuessed] = useState(false);
@@ -24,6 +24,7 @@ const Game = () => {
       onSubmit={() => setHasGuessed(true)}
       disabled={hasGuessed}
     />
+      {children}
     <GameStatus isWinner={isWinner} hasGuessed={hasGuessed} />
     <button
       onClick={() => {
